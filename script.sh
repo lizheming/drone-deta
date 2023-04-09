ebf90f74f925968d6152362de88e9844b1889740
lizheming <i@imnerd.org>
2023-04-09 14:26:29 +0800

    chore: chang -e to -n

--- script.sh
+++ script.sh
@@ -58,7 +58,7 @@ mkdir .space && echo "{\"id\":\"${SPACE_ID}\",\"name\":\"\",\"alias\":\"\"}" > .
 
 echo "> Start push your project to Deta space…"
 pushArgs=""
-if [ -e "$SPACE_PUSH_TAG" ]; then
+if [ -n "$SPACE_PUSH_TAG" ]; then
   pushArgs="$pushArgs --tag=$SPACE_PUSH_TAG"
 fi
 
@@ -79,19 +79,19 @@ echo "> Start release your build version"
 # revision=$(grep -oE 'created revision: .*$' ./push.log | awk '{print $3}')
 # releaseArgs="--rid=$revision"
 releaseArgs="--latest"
-if [ -e "$SPACE_LISTED" ]; then
+if [ -n "$SPACE_LISTED" ]; then
   releaseArgs="$releaseArgs --listed=$SPACE_LISTED"
 fi
-if [ -e "$SPACE_DIR" ]; then
+if [ -n "$SPACE_DIR" ]; then
   releaseArgs="$releaseArgs --dir=$SPACE_DIR"
 fi
-if [ -e "$SPACE_NOTES" ]; then
+if [ -n "$SPACE_NOTES" ]; then
   releaseArgs="$releaseArgs --notes=$SPACE_NOTES"
 fi
-if [ -e "$SPACE_RID" ]; then
+if [ -n "$SPACE_RID" ]; then
   releaseArgs="$releaseArgs --rid=$SPACE_RID"
 fi
-if [ -e "$SPACE_VERSION" ]; then
+if [ -n "$SPACE_VERSION" ]; then
   releaseArgs="$releaseArgs --version=$SPACE_VERSION"
 fi
 
