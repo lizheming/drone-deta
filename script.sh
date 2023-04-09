187b468144ab7456a004a939377210013ae2d970
lizheming <i@imnerd.org>
2023-04-09 11:27:43 +0800

    chore: use --latest flag

--- script.sh
+++ script.sh
@@ -64,7 +64,7 @@ fi
 
 echo "> push command: space push $pushArgs"
 
-/root/.detaspace/bin/space push $pushArgs | tee ./push.log
+/root/.detaspace/bin/space push $pushArgs # | tee ./push.log
 rc=$?;
 if [[ $rc != 0 ]];
 then 
@@ -76,8 +76,9 @@ fi
 
 echo "> Start release your build version"
 
-revision=$(grep -oE 'created revision: .*$' ./push.log | awk '{print $3}')
-releaseArgs="--rid=$revision"
+# revision=$(grep -oE 'created revision: .*$' ./push.log | awk '{print $3}')
+# releaseArgs="--rid=$revision"
+releaseArgs="--latest"
 if [ -e "$SPACE_LISTED" ]; then
   releaseArgs="$releaseArgs --listed=$SPACE_LISTED"
 fi
